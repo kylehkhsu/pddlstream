387a81f99849b2148cb81ee06b07fac7e571e95a
Kyle Hsu <kylehkhsu@gmail.com>
2021-07-13 16:28:59 -0700

    packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pddlstream',
+      version='0.0.0',
+      description='',
+      url='http://github.com/kylehkhsu/pddlstream',
+      author='',
+      author_email='kylehkhsu@gmail.com',
+      license='MIT',
+      packages=['pddlstream'],
+      zip_safe=False
+)
